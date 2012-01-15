dcf27a49cacdb4eac538306f3e9e837fd584135b
Marcin Kumorek <bez.niczego@gmail.com>
2012-01-15 22:33:50 +0100

    fixed DTAG in arrayz

--- HelloAndroid/src/com/example/helloandroid/Arrayz.java
+++ HelloAndroid/src/com/example/helloandroid/Arrayz.java
@@ -6,7 +6,7 @@ import android.util.Log;
 
 public class Arrayz extends Activity {
 
-	private static final String DEBUG_TAG = "DEBUG_TAG -> arrayz";
+	private static final String DTAG = Arrayz.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,23 +15,23 @@ public class Arrayz extends Activity {
 		String[] aColors = getResources().getStringArray(R.array.aColors);
 
 		for (int i = 0; i < aColors.length; i++) {
-			Log.v(DEBUG_TAG, "color#: " + i + " is " + aColors[i]);
+			Log.v(DTAG, "color#: " + i + " is " + aColors[i]);
 			String color = aColors[i];
 			char[] charArray = color.toCharArray();
 			for (int j = 0; j < charArray.length; j++) {
-				Log.v(DEBUG_TAG, "letter#: " + j + " -> " + charArray[j]);
+				Log.v(DTAG, "letter#: " + j + " -> " + charArray[j]);
 			}
 		}
 
 		for (String num : aColors) {
-			Log.v(DEBUG_TAG, num);
+			Log.v(DTAG, num);
 		}
 
 		int numColor = aColors.length;
-		Log.v(DEBUG_TAG, "numColor: " + numColor);
+		Log.v(DTAG, "numColor: " + numColor);
 
 		while (numColor > -1) {
-			Log.v(DEBUG_TAG, "color#: " + numColor + " is " + aColors[numColor]);
+			Log.v(DTAG, "color#: " + numColor + " is " + aColors[numColor]);
 			numColor--;
 		}
 
